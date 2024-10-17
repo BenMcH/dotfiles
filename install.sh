0507d9bc781c999f709cd39a27f98ea369d2ef3e
Ben McHone <ben@mchone.dev>
2024-10-16 22:10:36 -0500

    Rework install script

--- install.sh
+++ install.sh
@@ -1,20 +1,33 @@
-#! /bin/env bash
+#! /bin/env zsh
 
 git submodule init
 git submodule update
 
 # Ensure folders are not complete symlinks
-mkdir -p ~/.config/sway/
 mkdir -p ~/.ssh/
-mkdir -p ~/.aws/
-mkdir -p ~/.local/share/xfce4/terminal/colorschemes
 
-command -v starship > /dev/null && stow starship
-command -v sway > /dev/null && stow sway
-command -v nvim > /dev/null && stow nvim
-command -v aws > /dev/null && stow aws
 stow git
 stow ssh
 stow zsh
-stow tmux
-stow xfce4-terminal
+
+command -v tmux > /dev/null && stow tmux
+command -v starship > /dev/null && stow starship
+
+command -v nvim > /dev/null && stow nvim
+
+if command -v aws > /dev/null; then
+  mkdir -p ~/.aws/
+  stow aws
+fi
+
+if [ $(uname) = "Linux" ]; then
+  if command -v xfce4-terminal > /dev/null; then
+    mkdir -p ~/.local/share/xfce4/terminal/colorschemes
+    command -v xfce4-terminal > /dev/null && stow xfce4-terminal
+  fi
+  
+  if command -v sway > /dev/null; then
+    mkdir -p ~/.config/sway/
+    stow sway
+  fi
+fi
