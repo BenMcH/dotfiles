9d0d66a65ff099d9428a920db85061c686ee2ada
Ben McHone <ben@mchone.dev>
2024-10-19 22:51:13 -0500

    Add dracula theme

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -9,6 +9,7 @@ function command_exists -a "command"
 end
 
 if status is-interactive
+    fish_config theme choose "Dracula Official"
     set -gx PAGER bat
     command_exists "nvim" && set -gx EDITOR nvim
 
