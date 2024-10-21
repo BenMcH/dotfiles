875f326b192f86bfa00f8a31cce1c7a23787cca5
Ben McHone <ben@mchone.dev>
2024-10-21 07:36:50 -0500

    Fix mac fish issues

--- fish/dot-config/fish/conf.d/mac.fish
+++ fish/dot-config/fish/conf.d/mac.fish
@@ -1,4 +1,5 @@
 if status is-interactive && test (uname) = "Darwin"
-    . /opt/homebrew/opt/asdf/libexec/asdf.sh
+    fish_add_path /opt/homebrew/bin
+    source /opt/homebrew/opt/asdf/libexec/asdf.fish
     set -gx DOCKER_HOST "unix:///Users/bmchone/.colima/default/docker.sock"
 end
