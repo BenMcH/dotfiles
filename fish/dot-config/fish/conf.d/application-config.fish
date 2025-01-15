1f02df89b65db38c833c34b80e49176b2bde7b1e
Ben McHone <ben@mchone.dev>
2025-01-15 07:49:48 -0600

    Venv + fish reload

--- fish/dot-config/fish/conf.d/application-config.fish
+++ fish/dot-config/fish/conf.d/application-config.fish
@@ -18,3 +18,23 @@ end
 if test -f /opt/homebrew/opt/asdf/share/fish/vendor_completions.d/asdf.fish
     source /opt/homebrew/opt/asdf/share/fish/vendor_completions.d/asdf.fish
 end
+
+function venv
+    if test -n "$VIRTUAL_ENV"
+        deactivate
+        echo "Deactivated old venv"
+    end
+
+    if test -f ./.venv/bin/activate.fish
+        . ./.venv/bin/activate.fish
+    else if test -f ../.venv/bin/activate.fish
+        . ../.venv/bin/activate.fish
+    else
+        echo "No .venv found in this folder or up a level"
+        return
+    end
+
+    echo "Activated venv"
+end
+
+abbr fish-reload "source ~/.config/fish/**/*.fish"
