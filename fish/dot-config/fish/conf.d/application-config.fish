cc73db805f070b25c4f5b22a3a3781ff753c1475
Ben McHone <ben@mchone.dev>
2025-03-03 20:15:10 -0600

    remove pyenv

--- fish/dot-config/fish/conf.d/application-config.fish
+++ fish/dot-config/fish/conf.d/application-config.fish
@@ -1,11 +1,5 @@
 fish_config theme choose "Dracula Official"
 
-if command_exists "pyenv"
-    set -gx PYENV_ROOT "$HOME/.pyenv"
-    add_to_path_if_exists "$PYENV_ROOT/bin"
-    pyenv init - | source
-end
-
 command_exists "starship" && starship init fish | source
 command_exists "zoxide" && zoxide init fish | source
 command_exists "lsd" && abbr ls "lsd"
--- fish/dot-config/fish/conf.d/path.fish
+++ fish/dot-config/fish/conf.d/path.fish
@@ -4,5 +4,3 @@ add_to_path_if_exists "$HOME/.cargo/bin"
 add_to_path_if_exists "$HOME/.local/bin"
 add_to_path_if_exists "/usr/local/opt/dotnet@6/bin"
 
-set -gx PYENV_ROOT "$HOME/.pyenv"
-add_to_path_if_exists "$PYENV_ROOT/bin"
