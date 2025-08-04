3f93d64357b2b14f07c50e1124488e6ef14fc22a
Ben McHone <ben@mchone.dev>
2025-08-03 21:28:21 -0500

    Only enable fuzzel on linux

--- home-manager/dot-config/home-manager/home.nix
+++ home-manager/dot-config/home-manager/home.nix
@@ -44,20 +44,7 @@
     # '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-
     ".config/rofimoji.rc".text = ''
       selector = fuzzel
     '';
@@ -228,10 +215,9 @@
       };
     };
   };
-
-  # App selector
+  
   programs.fuzzel = {
-    enable = true;
+    enable = pkgs.stdenv.isLinux;
     
     settings = {
       colors = {
