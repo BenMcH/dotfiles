1c6f4abd4685a0650173a968fb1c31efe45bd6a6
Ben McHone <ben@mchone.dev>
2026-03-30 07:48:34 -0500

    Add yazi function

--- home-manager/dot-config/home-manager/home/mchone.nix
+++ home-manager/dot-config/home-manager/home/mchone.nix
@@ -432,6 +432,14 @@ in
 
         echo "Activated venv"
       '';
+      "y" = ''
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        command yazi $argv --cwd-file="$tmp"
+        if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+          builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+      '';
     };
 
     interactiveShellInit = ''
